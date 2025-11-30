af254c5ddddcab024ac913e7073f5fc8cf27c2c4
evrine <minayoevrine@gmail.com>
2025-12-01 00:53:34 +0300

    initial project setup

---
+++ backend/src/main/java/com/seccertificate/cert_generation/CertGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.seccertificate.cert_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertGenerationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seccertificate/cert_generation/CertGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seccertificate.cert_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
